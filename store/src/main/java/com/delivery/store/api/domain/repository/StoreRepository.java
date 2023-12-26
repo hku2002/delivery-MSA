0f0035710a27203cb57835b33c84354a1ab9d5ad
hku2002 <hku2002@hanmail.net>
2023-12-26 22:50:48 +0900

    feat: Repository 최초 추가

---
+++ store/src/main/java/com/delivery/store/api/domain/repository/StoreRepository.java
@@ -0,0 +1,9 @@
+package com.delivery.store.api.domain.repository;
+
+import com.delivery.store.api.domain.entity.Store;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface StoreRepository extends JpaRepository<Store, Long> {
+}
