3a79a039c6dd5316d871767eac0b041e033d50d8
hku2002 <hku2002@hanmail.net>
2023-12-29 19:04:40 +0900

    feat: connector 만드는 샘플 추가

---
+++ infra/make-connector.sh
@@ -0,0 +1,32 @@
+# /bin/bash
+
+curl --location --request POST 'http://localhost:8083/connectors' \
+--header 'Content-Type: application/json' \
+--data-raw '{
+  "name": "delivery-connector",
+  "config": {
+    "connector.class": "io.debezium.connector.mysql.MySqlConnector",
+    "tasks.max": "1",
+    "database.hostname": "mysql",
+    "database.port": "3306",
+    "database.user": "root",
+    "database.password": "1234",
+    "database.server.id": "1",
+    "database.server.name": "dbserver1",
+    "database.allowPublicKeyRetrieval": "true",
+    "database.include.list": "store.menu",
+    "database.history.kafka.bootstrap.servers": "kafka:9092",
+    "database.history.kafka.topic": "dbhistory.store.menu",
+    "key.converter": "org.apache.kafka.connect.json.JsonConverter",
+    "key.converter.schemas.enable": "true",
+    "value.converter": "org.apache.kafka.connect.json.JsonConverter",
+    "value.converter.schemas.enable": "true",
+    "transforms": "unwrap,addTopicPrefix",
+    "transforms.unwrap.type": "io.debezium.transforms.ExtractNewRecordState",
+    "transforms.addTopicPrefix.type":"org.apache.kafka.connect.transforms.RegexRouter",
+    "transforms.addTopicPrefix.regex":"(.*)",
+    "transforms.addTopicPrefix.replacement":"$1"
+  }
+}'
+
+
