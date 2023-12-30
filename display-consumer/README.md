f20c57b4a630c490a58aec247f133c9821ec5153
hku2002 <hku2002@hanmail.net>
2023-12-30 18:41:29 +0900

    feat: display-consumer 최초 추가

---
+++ display-consumer/README.md
@@ -0,0 +1,3 @@
+## display-consumer
+
+- 가게 전시 부분의 데이터를 가져오는 consumer-worker
---
+++ display-consumer/src/main/java/com/display/consumer/DisplayConsumerApplication.java
@@ -0,0 +1,13 @@
+package com.display.consumer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DisplayConsumerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DisplayConsumerApplication.class, args);
+    }
+
+}
