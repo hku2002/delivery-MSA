1eaf8e74992d8c407f7616b4862a67ba23282e12
hku2002 <hku2002@hanmail.net>
2023-12-30 23:25:29 +0900

    fix: 타입 에러 수정

--- display-consumer/src/main/java/com/display/consumer/listener/StoreListener.java
+++ display-consumer/src/main/java/com/display/consumer/listener/StoreListener.java
@@ -1,10 +1,6 @@
 package com.display.consumer.listener;
 
-import com.display.consumer.global.common.dto.BaseListenDto;
-import com.fasterxml.jackson.core.JsonProcessingException;
 import lombok.extern.slf4j.Slf4j;
-import org.apache.kafka.clients.consumer.ConsumerRecord;
-import org.apache.kafka.clients.consumer.ConsumerRecords;
 import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.stereotype.Component;
 
@@ -13,11 +9,8 @@ import org.springframework.stereotype.Component;
 public class StoreListener {
 
     @KafkaListener(topics = "mysql.store.outbox", groupId = "display-group")
-    public void consumeStoreOutboxTopic(ConsumerRecords<String, String> consumerRecords) throws JsonProcessingException {
-        for (ConsumerRecord<String, String> consumerRecord : consumerRecords) {
-            BaseListenDto listenDto = BaseListenDto.from(consumerRecord);
-            log.info("ListenDto : {}", listenDto);
-        }
+    public void consumeStoreOutboxTopic(String message) {
+        log.info("message: {}", message);
     }
 
 }
