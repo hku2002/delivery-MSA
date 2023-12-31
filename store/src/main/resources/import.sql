26bfca919abd64675aa001c6839bdf533770dd71
hku2002 <hku2002@hanmail.net>
2023-12-31 14:02:52 +0900

    feat: 배송비 필드 추가

--- store/src/main/resources/import.sql
+++ store/src/main/resources/import.sql
@@ -1,5 +1,5 @@
 /* 가게1 */
-INSERT INTO store (name, thumbnail_path, created_at) VALUES ('Store 1', 'http://localhost:8080/image/store/store1_thumbnail.jpg', now());
+INSERT INTO store (name, thumbnail_path, delivery_fee, created_at) VALUES ('Store 1', 'http://localhost:8080/image/store/store1_thumbnail.jpg', 3000, now());
 INSERT INTO menu (name, description, thumbnail_path, price, store_id, created_at) VALUES ('Store1 - Menu 1', 'Description for Menu 1', 'http://localhost:8080/image/store/store1_menu1_thumbnail.jpg', 8000, 1, now());
 INSERT INTO options (name, price, menu_id, created_at) VALUES ('옵션 추가1', 1000, 1, now());
 INSERT INTO options (name, price, menu_id, created_at) VALUES ('옵션 추가2', 500, 1, now());
@@ -8,7 +8,7 @@ INSERT INTO options (name, price, menu_id, created_at) VALUES ('감자 추가', 
 INSERT INTO options (name, price, menu_id, created_at) VALUES ('마늘 추가', 2000, 2, now());
 
 /* 가게2 */
-INSERT INTO store (name, thumbnail_path, created_at) VALUES ('Store 2', 'http://localhost:8080/image/store/store2_thumbnail.jpg', now());
+INSERT INTO store (name, thumbnail_path, delivery_fee, created_at) VALUES ('Store 2', 'http://localhost:8080/image/store/store2_thumbnail.jpg', 3500, now());
 INSERT INTO menu (name, description, thumbnail_path, price, store_id, created_at) VALUES ('Store2 - Menu 1', 'Description for Menu 1', 'http://localhost:8080/image/store/store2_menu1_thumbnail.jpg', 15000, 2, now());
 INSERT INTO options (name, price, menu_id, created_at) VALUES ('스프 추가', 2000, 3, now());
 INSERT INTO options (name, price, menu_id, created_at) VALUES ('국 추가', 2000, 3, now());
