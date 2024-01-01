e364eef7fe01ef73c59b7e1d4d3f27ed2e7e1893
hku2002 <hku2002@hanmail.net>
2024-01-01 23:56:25 +0900

    feat: producer 소스 최초 추가

--- order-worker/src/main/java/com/order/worker/sender/OrderSender.java
+++ order-worker/src/main/java/com/order/worker/sender/OrderSender.java
@@ -1,12 +1,28 @@
 package com.order.worker.sender;
 
 import com.order.worker.dto.OrderSendDto;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.stereotype.Component;
 
+@Slf4j
 @Component
+@RequiredArgsConstructor
 public class OrderSender {
 
-    public void orderSend(OrderSendDto sendDto) {
+    private final KafkaTemplate<String, OrderSendDto> kafkaTemplate;
+    private static final String ORDER_CREATE_TOPIC_NAME = "order.create";
 
+    public void orderSend(OrderSendDto sendDto) {
+//        CompletableFuture<SendResult<String, OrderSendDto>> future = kafkaTemplate.send(ORDER_CREATE_TOPIC_NAME, sendDto);
+//        future.whenComplete((result, ex) -> {
+//            if (ex == null) {
+//                log.info("order.create message send success!");
+//            }
+//            else {
+//                log.error("order.create message send fail!");
+//            }
+//        });
     }
 }
