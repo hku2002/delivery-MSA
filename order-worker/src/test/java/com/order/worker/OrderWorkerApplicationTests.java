16bfbdad9cd42aed9dd481e76c2946f8bd7bcc59
hku2002 <hku2002@hanmail.net>
2024-01-01 22:23:56 +0900

    feat: order-worker 최초 추가

---
+++ order-worker/src/test/java/com/order/worker/OrderWorkerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.order.worker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OrderWorkerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
