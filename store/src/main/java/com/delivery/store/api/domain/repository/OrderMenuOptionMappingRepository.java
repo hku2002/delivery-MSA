94077a478b8d463eaf8ef7c143f409d2549444ed
hku2002 <hku2002@hanmail.net>
2024-01-01 20:14:15 +0900

    fix: h2 -> mysql 수정

--- store/src/main/java/com/delivery/store/api/domain/repository/OrderMenuOptionMappingRepository.java
+++
@@ -1,9 +0,0 @@
-package com.delivery.store.api.domain.repository;
-
-import com.delivery.store.api.domain.entity.OrderMenuOptionMapping;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface OrderMenuOptionMappingRepository extends JpaRepository<OrderMenuOptionMapping, Long> {
-}
--- store/src/main/java/com/delivery/store/api/domain/repository/OrderRepository.java
+++
@@ -1,9 +0,0 @@
-package com.delivery.store.api.domain.repository;
-
-import com.delivery.store.api.domain.entity.Order;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface OrderRepository extends JpaRepository<Order, Long> {
-}
