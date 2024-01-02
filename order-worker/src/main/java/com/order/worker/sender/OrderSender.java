f171256c8c68b3fe9365257dfb9fcd3337a70997
hku2002 <hku2002@hanmail.net>
2024-01-02 21:55:14 +0900

    feat: 토픽 send 기능 추가

--- order-worker/src/main/java/com/order/worker/sender/OrderSender.java
+++ order-worker/src/main/java/com/order/worker/sender/OrderSender.java
@@ -4,7 +4,10 @@ import com.order.worker.dto.OrderSendDto;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.kafka.support.SendResult;
 import org.springframework.stereotype.Component;
+
+import java.util.concurrent.CompletableFuture;
 
 @Slf4j
 @Component
@@ -15,14 +18,14 @@ public class OrderSender {
     private static final String ORDER_CREATE_TOPIC_NAME = "order.create";
 
     public void orderSend(OrderSendDto sendDto) {
-//        CompletableFuture<SendResult<String, OrderSendDto>> future = kafkaTemplate.send(ORDER_CREATE_TOPIC_NAME, sendDto);
-//        future.whenComplete((result, ex) -> {
-//            if (ex == null) {
-//                log.info("order.create message send success!");
-//            }
-//            else {
-//                log.error("order.create message send fail!");
-//            }
-//        });
+        CompletableFuture<SendResult<String, OrderSendDto>> future = kafkaTemplate.send(ORDER_CREATE_TOPIC_NAME, sendDto);
+        future.whenComplete((result, ex) -> {
+            if (ex == null) {
+                log.info("order.create message send success!");
+            }
+            else {
+                log.error("order.create message send fail!");
+            }
+        });
     }
 }
