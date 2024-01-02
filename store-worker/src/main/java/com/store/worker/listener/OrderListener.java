1c9fee5110308f1d1070085bbdddfe894111ac18
hku2002 <hku2002@hanmail.net>
2024-01-02 23:11:03 +0900

    refactor: objectMapper 의존성 주입

--- store-worker/src/main/java/com/store/worker/listener/OrderListener.java
+++ store-worker/src/main/java/com/store/worker/listener/OrderListener.java
@@ -3,18 +3,21 @@ package com.store.worker.listener;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.store.worker.dto.OrderListenDto;
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.stereotype.Component;
 
 @Slf4j
 @Component
+@RequiredArgsConstructor
 public class OrderListener {
+
+    private final ObjectMapper objectMapper;
 
     @KafkaListener(topics = "order.create", groupId = "store-group")
     public void consumeOrderCreateTopic(String message) throws JsonProcessingException {
         log.info("message: {}", message);
-        ObjectMapper objectMapper = new ObjectMapper();
         OrderListenDto storeListenDto = objectMapper.readValue(message, OrderListenDto.class);
         log.info("orderId: {}", storeListenDto.getOrderId());
     }
