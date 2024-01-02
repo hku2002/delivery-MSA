47592b85d8bf8f02c864f2d5dcf20e0e252e4197
hku2002 <hku2002@hanmail.net>
2024-01-02 22:28:46 +0900

    feat: store-worker 최초 추가

---
+++ store-worker/src/main/java/com/store/worker/StoreWorkerApplication.java
@@ -0,0 +1,13 @@
+package com.store.worker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StoreWorkerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StoreWorkerApplication.class, args);
+    }
+
+}
