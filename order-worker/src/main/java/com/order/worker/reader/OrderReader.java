b4b53db7fe9097788df78c98cd4e50bb4d7d5a3e
hku2002 <hku2002@hanmail.net>
2024-01-02 22:08:08 +0900

    refactor: 로그 추가

--- order-worker/src/main/java/com/order/worker/reader/OrderReader.java
+++ order-worker/src/main/java/com/order/worker/reader/OrderReader.java
@@ -5,10 +5,12 @@ import com.order.worker.domain.enumtype.OrderStatus;
 import com.order.worker.domain.repository.OrderMenuOptionRepository;
 import com.order.worker.processor.OrderProcessor;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Component;
 
 import java.util.List;
 
+@Slf4j
 @Component
 @RequiredArgsConstructor
 public class OrderReader {
@@ -19,6 +21,7 @@ public class OrderReader {
     public void readRequestedOrder() {
         List<OrderMenuOption> orderMenuOptions = orderMenuOptionRepository.findAllByOrderStatus(OrderStatus.REQUEST);
         if (orderMenuOptions.isEmpty()) {
+            log.info("orderEmpty");
             return;
         }
         orderProcessor.createOrderSendDto(orderMenuOptions);
